70541195b0e56f23c46051b04d309ed80baa3a07
Corey Wilkins <coreyjwilkins@gmail.com>
2021-12-14 21:53:01 -0800

    add Bearer to auth header

--- GetSpotifyPlayerState/__init__.py
+++ GetSpotifyPlayerState/__init__.py
@@ -8,7 +8,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     url = "https://api.spotify.com/v1/me/player"
     logging.info('Python HTTP trigger function processed a request.')
     spotify_bearer_token = os.environ["spotify_bearer_token"]
-    headers = {'authorization': f'{spotify_bearer_token}'}
+    headers = {'authorization': f'Bearer {spotify_bearer_token}'}
     r = requests.get(url, headers= headers)
     return func.HttpResponse(
             r.text,
