66f1f769ad5c271d45ba66dea0d50d3ba7b1a9d9
Corey Wilkins <coreyjwilkins@gmail.com>
2021-12-14 21:42:13 -0800

    add SpotifyPlayer Function

---
+++ GetSpotifyPlayerState/__init__.py
@@ -0,0 +1,17 @@
+import logging
+import os
+import requests
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    url = "https://api.spotify.com/v1/me/player"
+    logging.info('Python HTTP trigger function processed a request.')
+    spotify_bearer_token = os.environ["spotify_bearer_token"]
+    headers = {'authorization': f'{spotify_bearer_token}'}
+    r = requests.get(url, headers= headers)
+    return func.HttpResponse(
+            r.text,
+            status_code=200
+    )
+
